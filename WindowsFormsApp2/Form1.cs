9955e98207ad88e93d8f639e062fb876efba10bf
Mr. L <t0132204.a3cae5c@m.evernote.com>
2020-09-22 14:07:56 +0800

    Switch Finish

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -10,6 +10,12 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp2
 {
+    enum DriverCommand
+    {
+        Start,
+        Stop
+    }
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,7 +25,22 @@ namespace WindowsFormsApp2
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Driver();
+        }
 
+        private void Driver(DriverCommand command)
+        {
+            switch(command)
+            {
+                case DriverCommand.Start:
+                    Console.WriteLine("Start!!!");
+                    break;
+                case DriverCommand.Stop:
+                    Console.WriteLine("Stop!!!");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
